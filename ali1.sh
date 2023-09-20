fab2f10c9e134d32692f24993539b74c36cbf7fb
zarcaster <120129134+zarcaster@users.noreply.github.com>
2023-09-20 07:36:36 -0400

    Update ali1.sh

--- ali1.sh
+++ ali1.sh
@@ -9,7 +9,7 @@ chmod 0600 /mnt/swapfile
 mkswap -U clear /mnt/swapfile
 swapon /mnt/swapfile
 
-pacstrap -K /mnt xorg amd-ucode base base-devel bluez bluez-utils curl dosfstools efibootmgr git gnome gnome-extra grub linux-firmware linux linux-headers man-db man-pages nano networkmanager ntfs-3g rsync texinfo wget cups cups-pdf openssh
+pacstrap -K /mnt xorg amd-ucode base base-devel bluez bluez-utils curl dosfstools efibootmgr git gnome gnome-extra grub linux-firmware linux linux-headers man-db man-pages nano networkmanager ntfs-3g rsync texinfo wget cups cups-pdf openssh ufw
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
